5490bf3938e9e82e2b331d6dd9f086d2e05c7ce1
piekstra <piekstra17@up.edu>
2015-03-15 03:21:14 -0700

    Added animation and more avatars
    The most important part of this update is that animations have been
    added for moving players. More avatars have also been added and they are
    now scaled to properly fit each tile. Repainting speed was increased by
    making the background stable (unchanging). Currently only the "graying"
    avatar can perform animations.

--- workspace/SquInt-GUI/src/Player.java
+++ workspace/SquInt-GUI/src/Player.java
@@ -11,8 +11,9 @@ public class Player {
 	public static final int RIGHT = 3;
 	
 	public static final int MOVE_DELAY = 250; // in milliseconds
+	public static final int NUM_PHASES = 4;
 	
-	public static final String DEFAULT_AVATAR = "vader";
+	public static final String DEFAULT_AVATAR = "green";
 
 	// Player attributes
 	public int x; 						// the x location of the player using tile coordinates
@@ -20,17 +21,22 @@ public class Player {
 	public int direction;
 	public String avatar = null;
 	public boolean allowedToMove;
+	public int animatePhase;
 	private Timer moveTimer = null;
 	
-	private List<String> avatars = Arrays.asList("vader");
+	private List<String> avatars = Arrays.asList(
+			"blond", "blond_spiky", "blonde_blue", "blonde_hairband", "blue", 
+			"brown", "brown_green", "brown_mustache", "ponytail", "dark_knight", 
+			"glasses", "graying", "green", "pink", "pirate", 
+			"purple", "red_mustache", "strawberry");
 	
 	public Player(int tile_x, int tile_y, int direction, String avatar, boolean canIMove) {
-		if (tile_x < 0 || tile_x > SquintMainWindow.TILES_DIM) {
+		if (tile_x < 0 || tile_x > SquintMainWindow.NUM_TILES_ACROSS) {
 			x = 0;
 		} else {
 			x = tile_x;
 		}
-		if (tile_y < 0 || tile_y > SquintMainWindow.TILES_DIM) {
+		if (tile_y < 0 || tile_y > SquintMainWindow.NUM_TILES_DOWN) {
 			y = 0;
 		} else {
 			y = tile_y;
@@ -45,7 +51,7 @@ public class Player {
 		} else {
 			this.avatar = DEFAULT_AVATAR;
 		}
-		
+		animatePhase = 0;
 		allowedToMove = canIMove;
 	}
 	
