417cb2f548467edde31d8c85ae58c3a328a9d79f
piekstra <piekstra17@up.edu>
2015-03-25 14:26:36 -0700

    Expansion of Mapper
    Added threading and program run time options. Improved design. Still
    needs fixing.
    Started working on prevent file overwrites.
    
    Overwrite:
    http://www.java2s.com/Tutorial/Java/0180__File/AvoidingOverwritingaFile.htm

--- workspace/SquInt-GUI/src/DirectoryXMLMapping.java
+++ workspace/SquInt-GUI/src/DirectoryXMLMapping.java
@@ -1,7 +1,10 @@
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -18,27 +21,23 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
 
-public class DirectoryXMLMapping {	
+public class DirectoryXMLMapping{	
 	
-	private static File directory;
-	private static Document doc;
-	private static Element root;
-	private static Element prevElement;
-	private static Element currElement;
-	private static Element nextElement;
-//	private static boolean in_subDir = false;
-//	private static Element prev_dir;
-
-	public DirectoryXMLMapping(String folder) {
-		directory = new File(folder);
-	}
+	private static Document doc = null;
+	private static Element root	= null;
+	private static Element prevElement = null;
+	private static Element nextElement = null;
+	private static final Boolean operate = true; 
+	private static final Boolean useThreading = false;	// this makes it a lot slower in my tests
+	private static final Boolean timeProgram = true;
+	public static long startTime = 0;
+	private static final Pattern lastIntPattern = Pattern.compile("[^0-9]+(([0-9]+)[)]?)$");
 
 	public static void main(String[] args) 
 	{
-		doc = null;
-		root = null;
-		prevElement = currElement = nextElement = null;
-		
+//		String rootDir = "C:\\users\\piekstra17\\";
+		String rootDir = "res\\";
+		System.out.println(rootDir);
 		try {	
 			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
@@ -47,91 +46,51 @@ public class DirectoryXMLMapping {
 		} catch (ParserConfigurationException pce) {
 			pce.printStackTrace();
 		}
-				
-//		DirectoryXMLMapping dirMapper = new DirectoryXMLMapping("res/");
 
+		
+		
+		if (timeProgram) {
+			startTime = System.currentTimeMillis();
+		}
+		
 		if (doc != null) {
-			mapDirectories("res/");			
+			mapDirectories(rootDir);			
 		}
 		
-		try {
-			printDocument(doc, System.out);		
-		} catch (Exception e) {
-			
+
+	    Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+	        public void run() {
+	    		if (timeProgram) {
+	    			final long endTime = System.currentTimeMillis();
+	    			System.out.println("Total execution time: " + (endTime - startTime) );
+	    		}
+	        }
+	    }, "Shutdown-thread"));
+		if (operate) {
+			printDocumentToConsole(doc, System.out);	
+			outputDocument(doc, rootDir, "dirMap");	
 		}
-		
-		
-		System.exit(0);
-		
-//		try {			 
-//			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-//			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-//
-//			// root elements
-//			Document doc = docBuilder.newDocument();	
-//			Element rootElement = doc.createElement("company");
-//			doc.appendChild(rootElement);
-//
-//			// staff elements
-//			Element staff = doc.createElement("Staff");
-//			rootElement.appendChild(staff);
-//
-//			// set attribute to staff element
-//			Attr attr = doc.createAttribute("id");
-//			attr.setValue("1");
-//			staff.setAttributeNode(attr);
-//
-//			// shorten way
-//			// staff.setAttribute("id", "1");
-//
-//			// firstname elements
-//			Element firstname = doc.createElement("firstname");
-//			firstname.appendChild(doc.createTextNode("yong"));
-//			staff.appendChild(firstname);
-//
-//			// lastname elements
-//			Element lastname = doc.createElement("lastname");
-//			lastname.appendChild(doc.createTextNode("mook kim"));
-//			staff.appendChild(lastname);
-//
-//			// nickname elements
-//			Element nickname = doc.createElement("nickname");
-//			nickname.appendChild(doc.createTextNode("mkyong"));
-//			staff.appendChild(nickname);
-//
-//			// salary elements
-//			Element salary = doc.createElement("salary");
-//			salary.appendChild(doc.createTextNode("100000"));
-//			staff.appendChild(salary);
-//
-//			// write the content into xml file
-//			TransformerFactory transformerFactory = TransformerFactory.newInstance();
-//			Transformer transformer = transformerFactory.newTransformer();
-//			DOMSource source = new DOMSource(doc);
-//			StreamResult result = new StreamResult(new File("C:\\file.xml"));
-//
-//			// Output to console for testing
-//			// StreamResult result = new StreamResult(System.out);
-//
-//			transformer.transform(source, result);
-//
-//			System.out.println("File saved!");
-//
-//		} catch (ParserConfigurationException pce) {
-//			pce.printStackTrace();
-//		} catch (TransformerException tfe) {
-//			tfe.printStackTrace();
-//		}
+//		
+//		System.exit(0);
 	}
 	
-	private static void mapDirectories(String top) {
+	private static void mapDirectories(String dirPath) {
+		// We need to establish the root of the document, which will be the directory at the end of the dirPath - path\\end\\
+		int lastIdx = lastIdx = (dirPath.substring(0, dirPath.lastIndexOf("\\"))).lastIndexOf("\\") +1;
+
 		// Initialize the root element
-		root = doc.createElement(top.substring(0, top.length() - 1));
+		root = doc.createElement(dirPath.substring(lastIdx, dirPath.length() - 1));
 		doc.appendChild(root);
 		prevElement = root;
+
+		File topDir = new File(dirPath);
 		
-		File topDir = new File(top);
-		dirRecursion(topDir);
+		if (useThreading) {
+			Thread t1 = new Thread(new Recursor(topDir));
+			t1.start();
+		} else {
+			dirRecursion(topDir);
+		}
 	}
 	
 	/**
@@ -152,7 +111,7 @@ public class DirectoryXMLMapping {
 		// If there are sub files
 		for ( File f : contents) {
 			// Print out the path to the subfile
-//			System.out.println(f.getAbsolutePath());
+			System.out.println(f.getAbsolutePath());
 
 			// create xml object for the subfile
 			nextElement = createXMLObject(f.getAbsolutePath());
@@ -160,11 +119,16 @@ public class DirectoryXMLMapping {
 			if (nextElement != null) {
 				prevElement.appendChild(nextElement);				
 			}
-			
+
 			// recurse on the subfiles
-			dirRecursion(f);
+			if (useThreading) {
+				Thread t2 = new Thread(new Recursor(f));
+				t2.start();
+			} else {
+				dirRecursion(f);
+			}
 		}
-		// if we are here then we have gone through all subfiles, then back out of the current directory and go up one (cd .. essentially)
+		// if we are here then we have gone through all subfiles, then back out of the current rootDirectory and go up one (cd .. essentially)
 		Node prevNode = prevElement.getParentNode();
 		if (prevNode instanceof Element) {
 			prevElement = (Element)prevNode;
@@ -184,31 +148,141 @@ public class DirectoryXMLMapping {
 //		System.out.println(objName + "\t");
 		
 		// Check if the object has a file extension of .png
-		lastIdx = objName.lastIndexOf(".png");
+		lastIdx = objName.lastIndexOf(".");
 		if (lastIdx != -1) {
 			// If so, modify the object name to give it a prefix of i_
 			origName = objName;
 			objName = "file";
 		}
-		Element returnElement = doc.createElement(objName);
-		// If we have a file and not a directory then set some attributes for the file
+		// Here, check for any invalid characters for naming and remove them
+		objName = objName.replace("$", "");
+		// Here, check if the name starts with an invalid character and add an underscore (_)
+		if (objName.startsWith(".") || Character.isDigit(objName.charAt(0))) {
+			objName = "_" + objName;
+		}
+
+		Element returnElement = null;
+		try {
+			returnElement = doc.createElement(objName);
+		} catch (Exception e){
+			System.out.println(e);
+		}
+		// If we have a file and not a rootDirectory then set some attributes for the file
 		if (objName == "file") {
 			returnElement.setAttribute("name", origName);
 		}
 		return returnElement;
 	}
 	
-	public static void printDocument(Document doc, OutputStream out) throws IOException, TransformerException {
-	    TransformerFactory tf = TransformerFactory.newInstance();
-	    Transformer transformer = tf.newTransformer();
-	    transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
-	    transformer.setOutputProperty(OutputKeys.METHOD, "xml");
-	    transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-	    transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-	    transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+	public static void printDocumentToConsole(Document doc, OutputStream out) {
+		try {
+		    TransformerFactory tf = TransformerFactory.newInstance();
+		    Transformer transformer = tf.newTransformer();
+		    transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+		    transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+		    transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+		    transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+		    transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
 
-	    transformer.transform(new DOMSource(doc), 
-	         new StreamResult(new OutputStreamWriter(out, "UTF-8")));
+		    transformer.transform(new DOMSource(doc), 
+		         new StreamResult(new OutputStreamWriter(out, "UTF-8")));			
+		} catch (TransformerException te) {
+            System.out.println(te.getMessage());
+        } catch (IOException ioe) {
+            System.out.println(ioe.getMessage());
+        }
+	}
+	
+	public static void outputDocument(Document doc, String saveLocation, String fileName) {
+		 try {
+            Transformer tr = TransformerFactory.newInstance().newTransformer();
+            tr.setOutputProperty(OutputKeys.INDENT, "yes");
+            tr.setOutputProperty(OutputKeys.METHOD, "xml");
+            tr.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+            tr.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, "roles.dtd");
+            tr.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+
+            // send DOM to file
+//            File outputFile = new File(saveLocation + fileName + ".xml");
+            // Make sure the file does not exist yet but if it does
+        	boolean checkAgain = true;
+        	long lastNumberInt = 0;
+        	
+        	String filepath = saveLocation + fileName + ".xml";
+            File aFile = new File(filepath);
+            FileOutputStream outputFile = null;
+            if (aFile.isFile()) {
+              File newFile = aFile;
+              do {
+                String name = newFile.getName();
+                int period = name.indexOf('.');
+                newFile = new File(newFile.getParent(), name.substring(0, period) + "_old"
+                    + name.substring(period));
+              } while (newFile.exists());
+              aFile.renameTo(newFile);
+            }
+            try {
+              outputFile = new FileOutputStream(aFile);
+              System.out.println("myFile.txt output stream created");
+            } catch (Exception e) {
+              e.printStackTrace(System.err);
+            }
+//        	while (checkAgain) {
+//        		checkAgain = false;
+//	            if (outputFile.exists()) {
+//                	// Check if the file name already has a number at the end
+//                	Matcher matcher = lastIntPattern.matcher(fileName);
+//                	if (matcher.find()) {
+//                	    String someNumberStr = matcher.group(1);
+//                	    boolean addParen = false;
+//                	    if (someNumberStr.endsWith(")")) {
+//                	    	someNumberStr = someNumberStr.substring(0, someNumberStr.length()-1);
+//                	    	addParen = true;
+//                	    }
+//                	    lastNumberInt = Integer.parseInt(someNumberStr);
+//                	    // rename the file to be the next number so we don't overwrite
+//                	    fileName = fileName.replace(matcher.group(1), "" + (lastNumberInt+1));
+//                	    if (addParen) {	
+//                	    	fileName += ")";
+//                	    	checkAgain = true;
+//                	    }
+//                	} else {
+//                		// If the fileName exists and does not have a number at the end of the name
+//                		// then add a number so we don't overwrite the existing file
+//                		if(fileName.contains("(1)")) {	
+//                			break;
+//                		}
+//            			fileName += " (1)";
+//                		checkAgain = true;	// If we rename the filename, check again that it doesn't exist
+//                	}
+//                	outputFile = 
+//            	}
+//            }
+            tr.transform(new DOMSource(doc), 
+                    new StreamResult(outputFile));
+//            tr.transform(new DOMSource(doc), 
+//                                 new StreamResult(new FileOutputStream(saveLocation + fileName + ".xml")));
+
+        } catch (TransformerException te) {
+            System.out.println(te.getMessage());
+        } 
+//		 catch (IOException ioe) {
+//            System.out.println(ioe.getMessage());
+//        }
+	}
+	
+	public static class Recursor implements Runnable {
+		File topDir = null;
+		
+		public Recursor(File f) {
+			topDir = f;
+		}
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			dirRecursion(topDir);
+		}
 	}
 
 }
