764f6544fb5bf4b3a32149ea5b8f8328b9a4d25c
piekstra <piekstra17@up.edu>
2015-03-30 00:38:15 -0700

    Working on texture mapping from .zip bundle

--- src/Texture.java
+++ src/Texture.java
@@ -1,140 +1,139 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 
 public final class Texture {
 	
-	// Default Image Extension
-	public static final String IMG_EXT = ".png";
-	public static final String HOUSE = "house/";
-	public static final String SHADOW = "shadows/";
-	public static final String FLOOR = "floor/";
-	public static final String WALL = "walls/";
-	public static final String WALL_DMG = "walls_dmg/";
-	public static final String O_OUTSIDE_HOUSE = "outside_house/";
-	public static final String O_DOOR = "door/";
-	public static final String O_GRASS = "grass/";
-	public static final String O_HOUSE = "house/";
-	public static final String O_PAVEMENT = "pavement/";
-	public static final String O_TREE = "tree/";
-	public static final String O_WINDOW = "window/";
-	public static final String TRANSPARENT = "transparent.png";
-
-	// Main Texture Paths
-	public static final String IMAGE_PATH = "res/images/";
-	
-	static public class Images 
-	{
-		public String path = IMAGE_PATH;
-		public String transparent = this.path + TRANSPARENT;
-		@Override
-	    public String toString() { return path; }
+	public static final String resourceLocation = "res/textures.zip";
 		
-		static public class House extends Images 
-		{
-			String path = super.path + HOUSE;
-			@Override
-		    public String toString() { return path; }
-			
-			static public class Floor extends House 
-			{
-				String path = super.path + FLOOR;
-				@Override
-			    public String toString() { return path; }
+	public Texture() {
+	    ZipFile zipFile = null;
+		try {
+			zipFile = new ZipFile(resourceLocation);
 
-			}
-			static public class Shadow extends House 
-			{
-				String path = super.path + SHADOW;	
-				@Override
-			    public String toString() { return path; }
-			}
-			static public class Wall extends House {
-				String path = super.path + WALL;	
-				@Override
-			    public String toString() { return path; }	
-			}
-			static public class WallDmg extends House {
-				String path = super.path + WALL_DMG;
-				@Override
-			    public String toString() { return path; }
-			}
-		}	
-		static public class OutsideHouse extends Images 
-		{
-			String path = super.path + O_OUTSIDE_HOUSE;
-			@Override
-		    public String toString() { return path; }
-			
-			static public class Door extends House
-			{
-				String path = super.path + O_DOOR;
-				@Override
-			    public String toString() { return path; }
-			}
-			static public class Grass extends House 
-			{
-				String path = super.path + O_GRASS;
-				@Override
-			    public String toString() { return path; }	
-			}
-			static public class Building extends House
-			{
-				String path = super.path + O_HOUSE;
-				@Override
-			    public String toString() { return path; }		
-			}
-			static public class Pavement extends House 
-			{
-				String path = super.path + O_PAVEMENT;
-				@Override
-			    public String toString() { return path; }
-			}
-			static public class Tree extends House 
-			{
-				String path = super.path + O_TREE;
-				@Override
-			    public String toString() { return path; }
-			}
-			static public class Window extends House 
-			{
-				String path = super.path + O_WINDOW;
-				@Override
-			    public String toString() { return path; }
-			}
+		    Enumeration<? extends ZipEntry> entries = zipFile.entries();
+		    while(entries.hasMoreElements()){
+		        ZipEntry entry = entries.nextElement();
+		        // every entry is a folder and file in the directory
+		        // could use this to look for a file
+		        if (entry.getName().endsWith(".xml")) {
+			        InputStream stream = zipFile.getInputStream(entry);
+			        loadTextures(stream);
+		        }
+		    }
+		    
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
-	/*
-	 * 
-			
-		
-	 */
-//	private static final String HOUSE_PATH = IMAGE_PATH + "house/";
-//	private static final String OUTSIDE_HOUSE_PATH = IMAGE_PATH + "outside_house/";
-//	
-//	// Sub Texture Paths - House Map
-//	public static final String H_FLOOR = HOUSE_PATH + "floor/";
-//	public static final String H_SHADOWS = HOUSE_PATH + "shadows/";
-//	public static final String H_WALL = HOUSE_PATH + "walls/";
-//	public static final String H_WALLS_DMG = HOUSE_PATH + "walls_dmg/";
-//	
-//	// Sub Texture Paths - Outside House Map
-//	public static final String OH_DOOR = OUTSIDE_HOUSE_PATH + "door/";
-//	public static final String OH_GRASS = HOUSE_PATH + "grass/";
-//	public static final String OH_HOUSE = OUTSIDE_HOUSE_PATH + "house/";
-//	public static final String OH_PAVEMENT = OUTSIDE_HOUSE_PATH + "pavement/";
-//	public static final String OH_TREE = OUTSIDE_HOUSE_PATH + "tree/";
-//	public static final String OH_WINDOW = OUTSIDE_HOUSE_PATH + "window/";
-//	
-//	// Individual Texture Paths
-//	public static final String LEFT_WALL = H_WALL + 		"12" 	+ IMG_EXT;
-//	public static final String RIGHT_WALL = H_WALL +		"13" 	+ IMG_EXT;
-//	public static final String TOP_WALL = H_WALL +			"1" 	+ IMG_EXT;
-//	public static final String BOTTOM_WALL =  H_WALL +		"18" 	+ IMG_EXT;
-//	public static final String FLOOR_1 = H_FLOOR + 			"1"		+ IMG_EXT;
-//	public static final String FLOOR_2 = "in_floor/2.png";
-//	public static final String FLOOR_3 = "in_floor/3.png";
-//	public static final String FLOOR_4 = "in_floor/4.png";
-//	public static final String FLOOR_5 = "in_floor/5.png";
 	
-	public Texture() {
+	public static void main(String[] args) {
+		Texture t = new Texture();
+	}
+	
+	/**
+	 * Expects an xml file input stream
+	 * The xml file will tell it where to look for textures
+	 * 
+	 * @param stream
+	 */
+	private void loadTextures(InputStream stream) {
+		DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder docBuilder = null;
+		try {
+			docBuilder = docBuilderFactory.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		}
+		Document doc = null;
+		try {
+//			doc = docBuilder.parse(new File("res\\xml\\Textures.xml"));
+			doc = docBuilder.parse(stream);
+		} catch (SAXException | IOException e) {
+			e.printStackTrace();
+			return;
+		}
+
+		getFilesFromDir("floor", doc);
+		
+		
+	}
+	
+	private void getFilesFromDir(String directoryName, Document doc) {
+		XPath xPath = XPathFactory.newInstance().newXPath();
+		XPathExpression expr = null;
+		try {
+			expr = xPath.compile("//file");
+		} catch (XPathExpressionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		NodeList nl = null;
+		try {
+			nl = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
+		} catch (XPathExpressionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		for (int i = 0; i < nl.getLength(); i++) {
+			if (nl.item(i).getParentNode().getAttributes().item(0).getNodeValue().contains(directoryName)) {
+				System.out.println(nl.item(i).getTextContent());
+			}
+		}
+		
+		// Gets the path to all files
+		NodeList nodes = doc.getElementsByTagName("file");
+		for(int i = 0; i < nodes.getLength(); i++) {
+			System.out.println(getFileURL(nodes.item(i)));
+		}
+		
+	}
+	
+	private String getFileURL(Node n) {
+		ArrayList<String> path = new ArrayList<String>();
+		while (n.getParentNode() != null) {
+			if (n.hasAttributes()) {
+				String folderName = n.getAttributes().item(0).getNodeValue();
+				if (!folderName.endsWith("\\")) {
+					folderName += "\\";
+				}
+				if (folderName != null) {
+					path.add(folderName);						
+				}			
+			}
+			n = n.getParentNode();
+		}
+		Collections.reverse(path);
+		String pathString = "";
+		for (String s : path)
+		{
+			pathString += s;	
+		}	
+		return pathString;
+	}
+	
+	private void getFileWithName(String fileName) {
 		
 	}
 
